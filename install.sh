ff068b61682f7b5195023b9847d3444ed631f516
Stephen Flee <steveflee@gmail.com>
2025-07-17 15:36:56 -0700

    switches to stow

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-chezmoi init git@github.com:fleeco/dotfiles.git
-chezmoi apply
+# Stow is so much cooler than chezmoi.  I prefer being able to just work in my dotfiles repo and see the changes rather
+# than having to constantly chezmoi apply
+stow -t ~ */
