91cf61a86328847464dd38638193fc49fae12abd
Stephen Flee <steveflee@gmail.com>
2025-09-18 06:56:22 -0700

    Adds some checks on zshrc

--- nvim/.config/nvim/lua/thelatestbuild/lazy/telescope.lua
+++ nvim/.config/nvim/lua/thelatestbuild/lazy/telescope.lua
@@ -4,6 +4,11 @@ return {
         tag = '0.1.8',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
+            require('telescope').setup({
+                defaults = {
+                    hidden = false,
+                },
+            })
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
             vim.keymap.set('n', '<C-p>', builtin.git_files, {})
